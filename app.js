7b7af56394c6e7714628c0687a425f14b2bec4ff
Aliaksei Bychyk <aliaksei_bychyk@epam.com>
2018-03-12 23:32:36 +0300

    homework 1

---
+++ app.js
@@ -0,0 +1,6 @@
+import config from './config/config.json'
+import { User, Product } from './models/model'
+
+console.log(config.name)
+new User('Aleks')
+new Product('Rooftop')
---
+++ models/model.js
@@ -0,0 +1,13 @@
+export class Product {
+  constructor (product) {
+    this.product = product
+    console.log(this.product)
+  }
+}
+
+export class User {
+  constructor (user) {
+    this.user = user
+    console.log(this.user)
+  }
+}
